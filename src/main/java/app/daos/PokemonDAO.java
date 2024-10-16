c6928b30f9c25ecd60bc6b80dd10e2ade0a564fb
Umair-230104 <cph-ut38@cphbusiness.dk>
2024-10-16 12:55:07 +0200

    ændret update metode i dao til og bruge pokemons klassens konverter metode

--- src/main/java/app/daos/PokemonDAO.java
+++ src/main/java/app/daos/PokemonDAO.java
@@ -2,12 +2,10 @@ package app.daos;
 
 import app.dtos.PokemonDetailDTO;
 import app.entities.Pokemon;
-import app.entities.Type;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class PokemonDAO implements IDAO
 {
@@ -46,37 +44,52 @@ public class PokemonDAO implements IDAO
         }
     }
 
-    public void updatePokemon(Pokemon pokemon, PokemonDetailDTO pokemonDetailDTO)
+    public void updatePokemon(PokemonDetailDTO pokemonDetailDTO)
     {
         try (var em = emf.createEntityManager())
         {
             em.getTransaction().begin();
 
-            // Update Pokémon's basic information
-            pokemon.setName(pokemonDetailDTO.getName());
-            pokemon.setHeight(pokemonDetailDTO.getHeight());
-            pokemon.setWeight(pokemonDetailDTO.getWeight());
-
-            // Clear current types and set new ones
-            pokemon.getTypes().clear();  // Clear existing types to prevent duplication
-
-            // Map the new types from the DTO and set the Pokemon reference
-            List<Type> updatedTypes = pokemonDetailDTO.getTypes().stream()
-                    .map(typeDTO ->
-                    {
-                        Type type = new Type(typeDTO.getType().getName(), pokemon);  // Set the pokemon reference here
-                        return type;
-                    })
-                    .collect(Collectors.toList());
-
-            // Set the updated types to the Pokemon
-            pokemon.setTypes(updatedTypes);
+            // Use the constructor to convert PokemonDetailDTO to Pokemon
+            Pokemon updatedPokemon = new Pokemon(pokemonDetailDTO);
 
             // Merge the updated Pokemon entity back into the database
-            em.merge(pokemon);
+            em.merge(updatedPokemon);
             em.getTransaction().commit();
         }
     }
+
+//    public void updatePokemon(Pokemon pokemon, PokemonDetailDTO pokemonDetailDTO)
+//    {
+//        try (var em = emf.createEntityManager())
+//        {
+//            em.getTransaction().begin();
+//
+//            // Update Pokémon's basic information
+//            pokemon.setName(pokemonDetailDTO.getName());
+//            pokemon.setHeight(pokemonDetailDTO.getHeight());
+//            pokemon.setWeight(pokemonDetailDTO.getWeight());
+//
+//            // Clear current types and set new ones
+//            pokemon.getTypes().clear();  // Clear existing types to prevent duplication
+//
+//            // Map the new types from the DTO and set the Pokemon reference
+//            List<Type> updatedTypes = pokemonDetailDTO.getTypes().stream()
+//                    .map(typeDTO ->
+//                    {
+//                        Type type = new Type(typeDTO.getType().getName(), pokemon);  // Set the pokemon reference here
+//                        return type;
+//                    })
+//                    .collect(Collectors.toList());
+//
+//            // Set the updated types to the Pokemon
+//            pokemon.setTypes(updatedTypes);
+//
+//            // Merge the updated Pokemon entity back into the database
+//            em.merge(pokemon);
+//            em.getTransaction().commit();
+//        }
+//    }
 
     public void savePokemonsToDb(List<PokemonDetailDTO> pokemonDetailDTOList)
     {
@@ -90,7 +103,6 @@ public class PokemonDAO implements IDAO
             }
         }
     }
-
 
 //    public void savePokemonsToDb(List<PokemonDetailDTO> pokemonDetailDTOList)
 //    {
